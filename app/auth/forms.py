6678d903c69ffcfc2b5f496f4664471e2ce211a2
Bruce Tran <brucetran323@gmail.com>
2023-04-01 18:00:55 -0400

    fixed more errors, focused on login based errors

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -8,7 +8,7 @@ class SignUpForm(FlaskForm):
     username = StringField('Username', validators = [DataRequired()])
     email = StringField('Email', validators = [DataRequired()])
     password = PasswordField('Password', validators = [DataRequired()])
-    confirm_password = PasswordField("Confirm", validators = [DataRequired(), EqualTo('password')])
+    confirm_password = PasswordField('Confirm Password', validators = [DataRequired(), EqualTo('password')])
     submit = SubmitField()
 
 class LoginForm(FlaskForm):
