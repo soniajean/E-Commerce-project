ebe290b7c65222b18530f8ce2b4dce8624a1a62f
Bruce Tran <brucetran323@gmail.com>
2023-04-01 20:06:12 -0400

    Models changed, any added icons to base html and nav bar

--- config.py
+++ config.py
@@ -8,4 +8,3 @@ class Config():
     SECRET_KEY = os.environ.get('SECRET_KEY')
     SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-## HELLO FROM BRUCE
